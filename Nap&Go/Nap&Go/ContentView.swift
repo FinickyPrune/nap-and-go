3c040cc1e584e0190506097d380e038ff9c1bccd
Anastasia Kravchenko <anastasia.kravchenko@sibers.com>
2022-08-11 12:14:50 +0700

    map added

--- Nap&Go/Nap&Go/ContentView.swift
+++ Nap&Go/Nap&Go/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 40.7,
+            longitude: -74),
+        span: MKCoordinateSpan(
+            latitudeDelta: 10,
+            longitudeDelta: 10)
+    )
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("So where we gonna go?")
+                    .bold()
+                    .font(.system(size: 27, weight: .heavy, design: .rounded))
+                    .background(.green)
+//                Spacer()
+                Map(coordinateRegion: $region)
+            }
+            .background(.blue)
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
